68cdc7ea4b97a8f461bbd703e345be405c2a256c
rafallopatka-star <rafal.lopatka@edu.uekat.pl>
2025-10-09 12:05:17 +0200

    Delete palindrome.py

--- palindrome.py
+++
@@ -1,9 +0,0 @@
-def is_palindrome(text: str) -> bool:
-    text = text.lower().replace(" ", "").replace(",", "").replace(".", "")
-    return text == text[::-1]
-
-print(is_palindrome('Kajak'))
-print(is_palindrome('Kobyła ma mały bok'))
-print(is_palindrome('python'))
-print(is_palindrome(''))
-print(is_palindrome('A'))
